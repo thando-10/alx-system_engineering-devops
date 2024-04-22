eecf8281ccb32d57c2aac96e9252bcb6311147b1
thando-10 <thandoxsndlovu@gmail.com>
2024-04-22 12:57:17 -0700

    secccond

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # creates a file
 file { '/tmp/school':
-        ensure	=> file,
-        mode	=> '0744',
-        owner	=> 'www-data',
-        group	=> 'www-data',
-        content	=> 'I love Puppet'
+  ensure	=> file,
+  content	=> 'I love Puppet',
+  mode		=> '0744',
+  owner		=> 'www-data',
+  group		=> 'www-data',
 }
 
